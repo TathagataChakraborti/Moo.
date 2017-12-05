69df1b408ddb336473f7efb35bb0bc2cacc78ae8
Sachin Grover <sachin.grover@asu.edu>
2017-12-05 12:06:25 -0700

    changes for migrating to flask

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from interface import plan, explain
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
+@app.route('/student')
 def root():
-	return render_template('index.html')
+	return render_template('student_interface.html')
+
+@app.route('/teacher')
+def teacher():
+	return render_template('instructor_interface.html')
 
 @app.route('/js/<path:path>')
 def send_js(path):
-	return send_from_directory('js', path)
+	return send_from_directory('static/js', path)
 
 @app.route('/css/<path:path>')
 def send_css(path):
-	return send_from_directory('css', path)
+	return send_from_directory('static/css', path)
+
+@app.route('/images/<path:path>')
+def send_images(path):
+	return send_from_directory('static/images', path)
 
 @app.route('/plan', methods = ['GET', 'POST'])
 def create_plan():
